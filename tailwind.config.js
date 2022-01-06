7514acdec98800c998d7693dffd8c7a9771f252c
benacq <benacq44@gmail.com>
2022-01-06 14:25:46 +0000

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+const { screens } = require('tailwindcss/defaultTheme')
+module.exports = {
+  mode: 'jit',
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens: {
+      'xsm': '480px',
+      ...screens
+    },
+
+    extend: {},
+  },
+  plugins: [],
+}
