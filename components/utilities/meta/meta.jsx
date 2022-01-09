ec43151eb7c8ebfff18b4234d5c2599147fb7f79
buabaj <buabajerry@gmail.com>
2022-01-09 11:50:05 +0000

    adds meta to index page

--- components/utilities/meta/meta.jsx
+++ components/utilities/meta/meta.jsx
@@ -14,7 +14,7 @@ const Meta = (props) => {
             <meta property="twitter:description" content="McMendel Energy is an energy provision and a global warming and climate change advocate company aspiring to change the face of energy using green energy. The Company is driven by its vision of making an impact in the energy sector of Ghana and Africa, which inspires, motivates and keeps the Firm focused on doing what really matters." />
             <meta name="keywords" content="McMendel Energy, renewable energy, sustainability, Ghana clean energy, Ghana renewable energy, McMendel Ghana Energy, KNUST Green Project, McMendel KNUST, Sustainability in Ghana" />
             <meta name="description" content="McMendel Energy is an energy provision and a global warming and climate change advocate company aspiring to change the face of energy using green energy. The Company is driven by its vision of making an impact in the energy sector of Ghana and Africa, which inspires, motivates and keeps the Firm focused on doing what really matters." />
-            <link rel='shortcut icon' href='/mcmendel.jpg' />
+            <link rel='shortcut icon' href='/mcmndl.png' />
 
         </Head>
     );
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Index from '../components/home'
 import Nav from '../components/utilities/nav/nav'
+import Meta from '../components/utilities/meta/meta'
 
 export default function Home() {
   return (
     <>
       {/* <Nav /> */}
+      <Meta title='McMendel Energy' />
       <Index />
     </>
   )
