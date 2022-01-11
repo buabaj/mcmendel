de207ec446129a75eeb7582256008f1ce7a1ccac
buabaj <buabajerry@gmail.com>
2022-01-11 11:42:36 +0000

    fixes meta data

--- components/utilities/meta/meta.jsx
+++ components/utilities/meta/meta.jsx
@@ -7,10 +7,15 @@ const Meta = (props) => {
             <title>{props.title}</title>
             <meta name="viewport" content="initial-scale=1.0, width=device-width" />
             <meta property="og:title" content={props.title} />
+            <meta property="og:type" content="website" />
+            <meta property="og:image" itemProp="image" content="https://i.ibb.co/1zNsK8G/mcmendel.jpg" />
+            <meta property="og:description" content="McMendel Energy is an energy provision and a global warming and climate change advocate company aspiring to change the face of energy using green energy. The Company is driven by its vision of making an impact in the energy sector of Ghana and Africa, which inspires, motivates and keeps the Firm focused on doing what really matters." />
+            <meta property="og:site_name" content={props.title} />
             <meta property="og:image" content="https://i.ibb.co/1zNsK8G/mcmendel.jpg" />
             <meta property="twitter:card" content="summary_large_image" />
             <meta property="twitter:title" content={props.title} />
             <meta name="twitter:image" content="https://i.ibb.co/1zNsK8G/mcmendel.jpg" />
+            <meta property="og:url" content="http://mcmendelenergy.com" />
             <meta property="twitter:description" content="McMendel Energy is an energy provision and a global warming and climate change advocate company aspiring to change the face of energy using green energy. The Company is driven by its vision of making an impact in the energy sector of Ghana and Africa, which inspires, motivates and keeps the Firm focused on doing what really matters." />
             <meta name="keywords" content="McMendel Energy, renewable energy, sustainability, Ghana clean energy, Ghana renewable energy, McMendel Ghana Energy, KNUST Green Project, McMendel KNUST, Sustainability in Ghana" />
             <meta name="description" content="McMendel Energy is an energy provision and a global warming and climate change advocate company aspiring to change the face of energy using green energy. The Company is driven by its vision of making an impact in the energy sector of Ghana and Africa, which inspires, motivates and keeps the Firm focused on doing what really matters." />
