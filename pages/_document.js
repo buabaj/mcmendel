627e24762cde455776a796b44d28aaca29575aaa
benacq <benacq44@gmail.com>
2022-06-06 21:56:01 +0000

    updates

--- pages/_document.js
+++ pages/_document.js
@@ -3,8 +3,14 @@ import Document, { Head, Html, Main, NextScript } from "next/document";
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
+          <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+          <meta
+            httpEquiv="Content-Type"
+            content="text/html; charset=ISO-8859-1"
+          />
+
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
             rel="preconnect"
