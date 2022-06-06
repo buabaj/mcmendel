09d6a66f4e7551803f7e95abd768d5dd36743a9f
benacq <benacq44@gmail.com>
2022-06-06 08:54:29 +0000

    update

--- components/hero-section/hero.jsx
+++ components/hero-section/hero.jsx
@@ -1,77 +1,17 @@
-import Image from "next/image";
-import Link from "next/link";
 import React from "react";
-import { IoMenuOutline } from "react-icons/io5";
-import { Link as ScrollLink, animateScroll as scroll } from "react-scroll";
+import Nav from "../nav/nav";
 
 function Hero({ cycleOpen }) {
-  const [dropdownOpen, setDropdownOpen] = React.useState(false);
   return (
     <div
       className="h-screen relative bg-[url('/renewable-5.jpg')] bg-cover bg-center bg-no-repeat"
       id={"home"}
     >
       <div className="absolute w-full h-full bg-custom-transparent z-20">
-        <nav>
-          <div className="relative my-4 h-10 w-20 md:h-16 md:w-28 ">
-            <Image layout="fill" src="/logo.png" alt="" />
-          </div>
-
-          <div>
-            <IoMenuOutline
-              size={40}
-              onClick={cycleOpen}
-              className="text-white block cursor-pointer md:hidden"
-            />
-          </div>
-
-          <ul className="gap-10 hidden md:flex">
-            <li>
-              <ScrollLink className="cursor-pointer" to="home" spy smooth>
-                HOME
-              </ScrollLink>
-            </li>
-            <li className="relative cursor-pointer">
-              <span onClick={() => setDropdownOpen((isOpen) => !isOpen)}>
-                PROJECTS
-              </span>
-              {dropdownOpen && (
-                <>
-                  <div className="bg-green-400 mt-2 w-48 h-20 absolute p-2 font-semibold">
-                    <ScrollLink
-                      className="cursor-pointer"
-                      to="green-project"
-                      spy
-                      smooth
-                      onClick={() => setDropdownOpen(false)}
-                    >
-                      The KNUST Green Project
-                    </ScrollLink>
-                  </div>
-                </>
-              )}
-            </li>
-            <li>
-              <ScrollLink className="cursor-pointer" to="about" spy smooth>
-                ABOUT
-              </ScrollLink>
-            </li>
-            <li>
-              <Link href="/executives" passHref>
-                <a>OUR TEAM</a>
-              </Link>
-            </li>
-
-            <li>
-              <ScrollLink className="cursor-pointer" to="footer" spy smooth>
-                CONTACT
-              </ScrollLink>
-            </li>
-          </ul>
-        </nav>
+        <Nav cycleOpen={cycleOpen} />
 
         <section className="padded">
-          <div className="mt-36 sm:mt-28 md:mt-10">
+          <div className="mt-36 sm:mt-28 md:mt-40">
             <h1 className="leading-hero-spaced relative font-bold text-white text-custom-md md:text-custom-xlg lg:text-custom-xxlg">
               <span className=" text-green-deep">Changing</span> the face
               <br /> of
